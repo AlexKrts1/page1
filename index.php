71489756fa9ffe47384d0e9cf287722e254a2c12
AlexKrts1 <picklesmail1@gmail.com>
2020-05-13 23:44:17 +0300

    Update index.php
    frame

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 
 	<TITLE>Супер Маркет РИКО </TITLE>
 </head>
-<FRAMESET border=4 cols="400, *">
+<FRAMESET border=4 cols="200, *">
 	<FRAME name=left marginWidth=5 marginHeight=5 src="	SM_Help_net\___left.htm" >
 	<FRAME name=body marginWidth=5 marginHeight=5 src="" >
 </FRAMESET>
