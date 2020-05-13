b18f059f31ae9b47a35470700a3d60b61ad6cd13
AlexKrts1 <picklesmail1@gmail.com>
2020-05-13 23:11:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,15 @@
 	
 <head>
 	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="title" content="SuperMarket — программа автоматизации для магазинов, супермаркетов">
+    <meta name="description" content="SuperMarket -программа автоматизации и управления учета в строительном магазине, магазине одежды,  комплексное решение. Бесплатно скачать демо версию">
+    <meta name="keywords" content="SuperMarket г.Херсон программа  купить автоматизация  склад сканеры штрихкода весы регистраторы кассовые аппараты терминал  управление  калькуляция учет технологическая фронт-офис бэк-офис">
+    <meta name="robots" content="index, follow">
+    <meta name="googlebot" content="index, follow">
+    <meta name="author" content="RikoSoft Васильев Алексей">
+
 	<TITLE>Супер Маркет РИКО </TITLE>
 </head>
 <FRAMESET border=4 cols="400, *">
