458dc7d83860f05d3eb2c91187d2e5b229ae03c7
AlexKrts1 <picklesmail1@gmail.com>
2020-05-13 22:28:50 +0300

    qqqqwe

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	
 <head>
 	<meta charset="UTF-8">
-	<TITLE>SM_Help_net.chm</TITLE>
+	<TITLE>Супер Маркет РИКО </TITLE>
 </head>
 <FRAMESET border=4 cols="400, *">
 	<FRAME name=left marginWidth=5 marginHeight=5 src="	SM_Help_net\___left.htm" >
